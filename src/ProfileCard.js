7610902a2d62751b81e2f6d78ca19e37f7aa1ff6
Sam Chillcott <sam@secret-source.eu>
2021-03-06 09:57:05 +0000

    move assets

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,14 +1,35 @@
 import React from 'react'
-
+import './'
 const ProfileCard = () => {
     return (
-        <div>
-            Victor Crest 26
+        <div className="profile-card">
+            <svg src="./assets/bg-pattern-card.svg" alt=""/>
+
+            <div className="profile-details">
+             Victor Crest 26
             London
+
+            </div>
+            <div className="stat-wrapper">
+            <div className="followers">
             80K Followers
+
+            </div>
+            <div className="likes">
             803K Likes
+
+
+            </div>
+            <div className="photos">
             1.4K Photos
-            Challenge by Frontend Mentor. Coded by Your Name Here.
+
+
+            </div>
+
+            </div>
+            <footer>
+                Challenge by Frontend Mentor. Coded by Sam C.
+            </footer>
         </div>
     )
 }
