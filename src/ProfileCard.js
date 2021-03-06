ab6a83dec4411c08f6e622ae6dd47cd5e935d776
Sam Chillcott <sam@secret-source.eu>
2021-03-06 09:24:26 +0000

    Add assets

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -3,7 +3,12 @@ import React from 'react'
 const ProfileCard = () => {
     return (
         <div>
-            Profile Card
+            Victor Crest 26
+            London
+            80K Followers
+            803K Likes
+            1.4K Photos
+            Challenge by Frontend Mentor. Coded by Your Name Here.
         </div>
     )
 }
