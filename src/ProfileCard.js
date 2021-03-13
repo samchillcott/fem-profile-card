b7908782e21bcc2f2ea778e796c5953cda3b1b1a
Sam Chillcott <sam@secret-source.eu>
2021-03-13 09:00:45 +0000

    Scaffolding update

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -2,32 +2,28 @@ import './ProfileCard.css'
 
 const ProfileCard = () => {
     return (
-        <div className="profile-card">
-            <svg src="./assets/bg-pattern-card.svg" alt=""/>
-
-            <div className="profile-details">
-                <img src="./assets/image-victor.jpg" className="profile-image" alt=""/>
-             Victor Crest 26
-            London
-
-            </div>
-            <div className="stat-wrapper">
-                <div className="followers">
-                80K Followers
-
+        <div className="card">
+            <header>
+                <div className="avatar"></div>
+            </header>
+            <h2>Victor Crest
+                <div className="age">26</div>
+            </h2>
+            <div className="label">London</div>
+            <footer>
+                <div className="col">
+                    <h3>80K</h3>
+                    <small>Followers</small>
                 </div>
-                <div className="likes">
-                803K Likes
-
-
+                <div className="col">
+                    <h3>803K</h3>
+                    <small>Likes</small>
                 </div>
-                <div className="photos">
-                1.4K Photos
-
-
+                <div className="col">
+                    <h3>1.4K</h3>
+                    <small>Photos</small>
                 </div>
-
-            </div>
+            </footer>
             <footer>
                 Challenge by Frontend Mentor. Coded by Sam C.
             </footer>
