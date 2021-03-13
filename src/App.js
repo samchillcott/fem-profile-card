2b54e2a5d2ea3945c6814c829a009883a33d4d0a
Sam Chillcott <sam@secret-source.eu>
2021-03-13 09:38:50 +0000

    strip back to app.js only

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-import ProfileCard from './ProfileCard';
 
 function App() {
   return (
     <div className="App">
-      <ProfileCard />
+      <main>
+      <div className="card">
+            <header>
+                <div className="avatar"></div>
+            </header>
+            <h2>Victor Crest
+                <div className="age">26</div>
+            </h2>
+            <div className="label">London</div>
+            <footer>
+                <div className="col">
+                    <h3>80K</h3>
+                    <small>Followers</small>
+                </div>
+                <div className="col">
+                    <h3>803K</h3>
+                    <small>Likes</small>
+                </div>
+                <div className="col">
+                    <h3>1.4K</h3>
+                    <small>Photos</small>
+                </div>
+            </footer>
+            <footer>
+                Challenge by Frontend Mentor. Coded by Sam C.
+            </footer>
+        </div>
+        </main>
     </div>
   );
 }
