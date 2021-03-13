89003ee1daea2fec8722441582e9f88a6f37bef8
Sam Chillcott <sam@secret-source.eu>
2021-03-13 12:01:32 +0000

    add font & sort footer

--- src/App.js
+++ src/App.js
@@ -8,27 +8,30 @@ function App() {
             <header>
                 <div className="avatar"></div>
             </header>
-            <h2>Victor Crest
+            <div className="content">
+              <h2>Victor Crest
                 <div className="age">26</div>
-            </h2>
-            <div className="label">London</div>
+              </h2>
+              <div className="label">London</div>
+            </div>
             <footer>
-                <div className="col">
-                    <h3>80K</h3>
-                    <small>Followers</small>
-                </div>
-                <div className="col">
-                    <h3>803K</h3>
-                    <small>Likes</small>
-                </div>
-                <div className="col">
-                    <h3>1.4K</h3>
-                    <small>Photos</small>
-                </div>
+              <div className="col">
+                  <h3>80K</h3>
+                  <small>Followers</small>
+              </div>
+              <div className="col">
+                  <h3>803K</h3>
+                  <small>Likes</small>
+              </div>
+              <div className="col">
+                  <h3>1.4K</h3>
+                  <small>Photos</small>
+              </div>
             </footer>
-            <footer>
-                Challenge by Frontend Mentor. Coded by Sam C.
-            </footer>
+            {/* <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+     Coded by <a href="#">Sam Chillcott</a>.
+  </div> */}
         </div>
         </main>
     </div>
